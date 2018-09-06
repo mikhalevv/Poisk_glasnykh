e2398aba60a94665707cc9927b09ed8188777ac2
Valentin <mikhalev88@icloud.com>
2018-09-06 15:31:04 +0300

    123

---
+++ Poisk_glasnykh.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  5 12:23:14 2018
+
+@author: mihalev
+"""
+s=input()
+vowels = 0
+for i in s:
+    letter = i.lower()
+    if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u" or letter == "y":
+        vowels +=1
+print('Number of vowels: ' + str(vowels))
+
+#ntcn
+    
